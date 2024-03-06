aad5b300d42799fd3f26eccbbda7972c3b6104e2
Jamesalot20 <123522216+Jamesalot20@users.noreply.github.com>
2024-03-05 23:18:14 -0600

    Update and rename index.js to bmiCalculator.js

---
+++ bmiCalculator.js
@@ -0,0 +1,13 @@
+function calculateBMI(heightInInches, weightInPounds) {
+    const heightInMeters = heightInInches * 0.0254;
+    const weightInKilograms = weightInPounds * 0.453592;
+    const bmi = weightInKilograms / (heightInMeters * heightInMeters);
+    return bmi;
+}
+
+function getBMICategory(bmi) {
+    if (bmi < 18.5) return 'Underweight';
+    else if (bmi >= 18.5 && bmi <= 24.9) return 'Normal weight';
+    else if (bmi >= 25 && bmi <= 29.9) return 'Overweight';
+    else return 'Obese';
+}
